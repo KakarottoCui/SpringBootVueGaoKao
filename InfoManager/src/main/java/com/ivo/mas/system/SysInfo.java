b90fa5839bdbe673338b47e40a950e856422e729
cuifengbo <471363336@qq.com>
2023-07-31 21:45:42 +0800

    2

--- InfoManager/src/main/java/com/ivo/mas/system/SysInfo.java
+++ InfoManager/src/main/java/com/ivo/mas/system/SysInfo.java
@@ -3,8 +3,8 @@ package com.ivo.mas.system;
 public class SysInfo {
 
     public static final String WX_GET_KEY = "https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code";
-    public static final String APP_SECRET = "33686d15edcf0fb040964015b0eb7a5c";
-    public static final String APP_ID = "wx077e6a4b181b4ba7";
+    public static final String APP_SECRET = "";
+    public static final String APP_ID = "";
 
     public static final String SYS_NAME = "MyQuestion";
     public static final String SYS_HEADER = "MyQuestion_SERVICE_REQUEST";
